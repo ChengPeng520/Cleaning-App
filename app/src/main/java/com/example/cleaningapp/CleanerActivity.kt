27e0fd4ef3d16d705bc8f57091720119f88056a1
fatruei <kk0128k@gmail.com>
2023-05-05 23:34:45 +0800

    修改清潔人員menu id、修改訂單狀態列style、新增清潔人員activity 加入 bvn、新增color for bvn

--- app/src/main/java/com/example/cleaningapp/CleanerActivity.kt
+++ app/src/main/java/com/example/cleaningapp/CleanerActivity.kt
@@ -2,10 +2,23 @@ package com.example.cleaningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CleanerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cleaner)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        val bvn = findViewById<BottomNavigationView>(R.id.bvn_cleaner)
+        // 頁面切換可以整合BottomNavigationView功能
+        NavigationUI.setupWithNavController(
+            bvn,
+            navHostFragment.navController
+        )
     }
 }
