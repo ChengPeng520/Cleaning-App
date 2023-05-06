d4e54507fdacf4a6f72fd140e235621aed547251
fatruei <kk0128k@gmail.com>
2023-05-06 22:43:29 +0800

    新增nav_menu4張圖片、實作會員登入內兩個button連接畫面、修改member細節

--- app/src/main/java/com/example/cleaningapp/CleanerActivity.kt
+++ app/src/main/java/com/example/cleaningapp/CleanerActivity.kt
@@ -2,23 +2,35 @@ package com.example.cleaningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.example.cleaningapp.databinding.ActivityCleanerBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class CleanerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCleanerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cleaner)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_cleaner)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
         val bvn = findViewById<BottomNavigationView>(R.id.bvn_cleaner)
-        // 頁面切換可以整合BottomNavigationView功能
+
         NavigationUI.setupWithNavController(
             bvn,
             navHostFragment.navController
         )
+
+        navHostFragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
+
+        }
     }
 }
