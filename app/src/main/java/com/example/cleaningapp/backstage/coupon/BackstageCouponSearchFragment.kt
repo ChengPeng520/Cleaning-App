fcce22e32d431d4423fa19597ca05975eb0b5159
Ciyi <fc09890213402@gmail.com>
2023-05-08 17:01:55 +0800

    新增coupon 更新BackstageOrderDetail 、BackstageActivity、ciyi_strings.xml

---
+++ app/src/main/java/com/example/cleaningapp/backstage/coupon/BackstageCouponSearchFragment.kt
@@ -0,0 +1,41 @@
+package com.example.cleaningapp.backstage.coupon
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.cleaningapp.R
+import com.example.cleaningapp.databinding.FragmentCouponSearchBinding
+
+
+class BackstageCouponSearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentCouponSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        requireActivity().setTitle(R.string.menu_backstage_orderManage)
+       binding = FragmentCouponSearchBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.setTitle(R.string.menu_backstage_couponsMange)
+        with(binding){
+            btCouponAdd.setOnClickListener{
+                Navigation.findNavController(it)
+                    .navigate((R.id.action_couponSearchFragment_to_backstageCouponCreatFragment))
+
+            }
+        }
+    }
+
+
+}
