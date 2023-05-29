fee7381f8d908c8812536a67f1b5fe096617066b
Vicky <vicky12280603@gmail.com>
2023-05-29 12:40:29 +0800

    ClearPhoto 相機新增照片已完成、OrderCsSignCheck電子簽名已完成、CleanerFrontOrderDetail、OrderOrdermatch假資料連動已完成

--- app/src/main/java/com/example/cleaningapp/cleaner/view/order/OrderOrdermatchFragment.kt
+++ app/src/main/java/com/example/cleaningapp/cleaner/view/order/OrderOrdermatchFragment.kt
@@ -1,33 +1,38 @@
 package com.example.cleaningapp.cleaner.view.order
 
-import androidx.lifecycle.ViewModelProvider
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cleaningapp.R
+import androidx.fragment.app.viewModels
+import com.example.cleaningapp.cleaner.uistate.Work
 import com.example.cleaningapp.cleaner.viewmodel.order.OrderOrdermatchViewModel
+import com.example.cleaningapp.databinding.FragmentVickyOrderOrdermatchBinding
 
 class OrderOrdermatchFragment : Fragment() {
+    private lateinit var binding: FragmentVickyOrderOrdermatchBinding
+    private val viewModel: OrderOrdermatchViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = OrderOrdermatchFragment()
-    }
-
-    private lateinit var viewModel: OrderOrdermatchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_vicky_order_ordermatch, container, false)
+    ): View {
+        binding = FragmentVickyOrderOrdermatchBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OrderOrdermatchViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            arguments?.let { bundle ->
+                bundle.getSerializable("order")?.let {
+                    binding.viewModel?.order?.value = it as Work
+                }
+        }
     }
-
 }
+
+
--- app/src/main/java/com/example/cleaningapp/cleaner/viewmodel/search/CleanerFrontOrderDetailViewModel.kt
+++ app/src/main/java/com/example/cleaningapp/cleaner/viewmodel/search/CleanerFrontOrderDetailViewModel.kt
@@ -4,25 +4,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.cleaningapp.R
 import com.example.cleaningapp.cleaner.uistate.Job
-import com.example.cleaningapp.cleaner.uistate.OrderChatroomItemUiState
-import com.example.cleaningapp.cleaner.uistate.OrderStateUiState
-import com.example.cleaningapp.cleaner.uistate.Orderdetail
-import kotlinx.coroutines.flow.MutableStateFlow
 
 
 class CleanerFrontOrderDetailViewModel  : ViewModel() {
-    val order: MutableLiveData<Orderdetail> by lazy { MutableLiveData<Orderdetail>()}
-    val cleaner :  MutableLiveData<Job> by lazy { MutableLiveData<Job>() }
 
-    init {
-        loadOrder()
+    val cleaner: MutableLiveData<Job> by lazy { MutableLiveData<Job>() }
+
     }
-    private fun loadOrder() {
-        order.value = Orderdetail(
-            R.drawable.cs_img_xxx,"王先生", 12345678, "臺北市", "中山區", "臺北市中山區南京東路三段219號5樓",
-            "2023年4月26日", "12:00-14:00", "12:00", "14:00",5.0,5.0,5.0,5.0,
-            "工具:  吸塵器 、拖把\n" +
-                    "整理重點:  臥房、客房 ", null, 1111, 0,
-            )
-    }
-}
+
