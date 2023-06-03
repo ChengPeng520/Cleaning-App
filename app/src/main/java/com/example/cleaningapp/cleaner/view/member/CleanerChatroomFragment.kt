d5968acea58183722a1017f1c878b95a1ae1474f
Fatruei <kk0128k@gmail.com>
2023-06-03 20:42:14 +0800

    清潔人員對後台聊天室完成

--- app/src/main/java/com/example/cleaningapp/cleaner/view/member/CleanerChatroomFragment.kt
+++ app/src/main/java/com/example/cleaningapp/cleaner/view/member/CleanerChatroomFragment.kt
@@ -1,11 +1,11 @@
 package com.example.cleaningapp.cleaner.view.member
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cleaningapp.R
@@ -43,9 +43,8 @@ class CleanerChatroomFragment : Fragment() {
                 LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             (rvContactWindowTalk.layoutManager as LinearLayoutManager).stackFromEnd = true
             rvContactWindowTalk.setItemViewCacheSize(500)
-            viewModel?.fetchChatRoomTalkList()
             viewModel?.uiState?.observe(viewLifecycleOwner) {
-                (rvContactWindowTalk.adapter as CleanerChatroomAdapter).submitList(it.chatroomItems.toMutableList())
+                 (rvContactWindowTalk.adapter as CleanerChatroomAdapter).submitList(it.chatroomItems.toMutableList())
                 rvContactWindowTalk.smoothScrollToPosition((rvContactWindowTalk.adapter as CleanerChatroomAdapter).itemCount)
             }
         }
