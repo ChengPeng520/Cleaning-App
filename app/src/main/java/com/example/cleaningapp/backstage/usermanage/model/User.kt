c4e42ab0f0e4620413da40a57d17f1788607fbf9
Albert <super870520@gmail.com>
2023-06-05 17:47:40 +0800

    後台 客訴管理：點選同意/拒絕後跳出確認頁面 用戶管理：新增main資料連線、mainDetail資料連線

--- app/src/main/java/com/example/cleaningapp/backstage/usermanage/model/User.kt
+++ app/src/main/java/com/example/cleaningapp/backstage/usermanage/model/User.kt
@@ -1,25 +1,66 @@
 package com.example.cleaningapp.backstage.usermanage.model
 
+import android.graphics.Bitmap
+import com.example.cleaningapp.share.ImageUtils
 import java.io.Serializable
+import java.sql.Timestamp
 
-class User(
-    var id: Int,
-    var avatar: Int,
-    var email: String,
-    var password: String,
-    var name: String,
-    var phone: String,
-    var address: String,
-    var gender: String,
-    var createTime: String,
-    var updateTime: String,
-    var role: String,
-    var suspend:String,
-    var verify: String,
-    var intro: String,
-    var idNum: String,
-    var bank: String,
-    var idCardFront: Int,
-    var idCardBack: Int,
-    var criminalRecord: Int,
-) : Serializable
+data class User(
+    var cleanerId: Int? = null,
+    var customerId: Int? = null,
+    var backstageId: Int? = null,
+    var email: String = "",
+    var name: String = "",
+    var photo: Bitmap? = null,
+    var phone: String = "",
+    var gender: Int = 0,
+    var introduction: String? = "",
+    var timeCreate: Timestamp,
+    var timeUpdate: Timestamp,
+    var role: String= "",
+    var suspend: Boolean = false,
+    var verify: Boolean = false,
+    var identifyNumber: String = "",
+    var idCardFront: Bitmap? = null,
+    var idCardBack: Bitmap? = null,
+    var crc: Bitmap? = null,
+) : Serializable {
+//    val userPhoto: Bitmap?
+//        get() {
+//            return ImageUtils.bytesToBitmap(photo!!)
+//        }
+
+    var userGender: String? = ""
+        get() {
+            return if (gender == 0) {
+                "男"
+            } else {
+                "女"
+            }
+        }
+
+//    val userIdCardFront: Bitmap?
+//        get() {
+//            idCardFront?.let {
+//                return ImageUtils.bytesToBitmap(it)
+//            }
+//            return null
+//        }
+//
+//    val userIdCardBack: Bitmap?
+//        get() {
+//            idCardBack?.let {
+//                return ImageUtils.bytesToBitmap(it)
+//            }
+//            return null
+//        }
+//
+//    val userCrc: Bitmap?
+//        get() {
+//            idCardBack?.let {
+//                return ImageUtils.bytesToBitmap(it)
+//            }
+//            return null
+//        }
+}
+
