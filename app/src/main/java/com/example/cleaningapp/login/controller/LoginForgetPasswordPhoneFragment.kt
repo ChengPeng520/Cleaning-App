bbdd751b46d70c0802e616db1d67a5030631a9d5
Rona <ejizjm1102@gmail.com>
2023-06-05 13:55:48 +0800

    新增firebase gradle/ 新增電話忘記密碼驗證(未完成)

--- app/src/main/java/com/example/cleaningapp/login/controller/LoginForgetPasswordPhoneFragment.kt
+++ app/src/main/java/com/example/cleaningapp/login/controller/LoginForgetPasswordPhoneFragment.kt
@@ -2,19 +2,31 @@ package com.example.cleaningapp.login.controller
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.cleaningapp.R
 import com.example.cleaningapp.databinding.FragmentRonaLoginForgetPasswordPhoneBinding
 import com.example.cleaningapp.login.viewModel.LoginForgetPasswordPhoneViewModel
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.*
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
-class LoginForgetPasswordPhoneFragment : Fragment() {
+class  LoginForgetPasswordPhoneFragment : Fragment() {
     private lateinit var binding: FragmentRonaLoginForgetPasswordPhoneBinding
+    private val myTag = "TAG_${javaClass.simpleName}"
+    private lateinit var auth: FirebaseAuth
+    private lateinit var verificationId: String
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,13 +49,14 @@ class LoginForgetPasswordPhoneFragment : Fragment() {
                     .navigate(R.id.action_loginForgetPasswordPhoneFragment_to_loginForgetPasswordEmailFragment)
             }
             btnForgetPasswordSend.setOnClickListener {
-                //1.手機寄送簡訊驗證通知
+                //1.發送驗證需求給手機
                 //2.轉跳至驗證頁面
-                if (!inputCheck()){
-                    return@setOnClickListener
+                if (inputCheck()){
+                    requestVerificationCode("+886${viewModel?.phone?.value}")
+                    Navigation.findNavController(it)
+                        .navigate(R.id.action_loginForgetPasswordPhoneFragment_to_forgetPasswordPhoneVFragment)
                 }
-                Navigation.findNavController(it)
-                    .navigate(R.id.action_loginForgetPasswordPhoneFragment_to_forgetPasswordPhoneVFragment)
+
             }
         }
     }
@@ -61,5 +74,53 @@ class LoginForgetPasswordPhoneFragment : Fragment() {
         }
             return check
     }
+
+    // 發送驗證需求給手機
+    private fun requestVerificationCode(mobile: String) {
+        auth.setLanguageCode("zh-Hant")
+        val phoneAuthOptions = PhoneAuthOptions.newBuilder(auth)
+            // 輸入的電話號碼
+            .setPhoneNumber(mobile)
+            // 驗證碼失效時間，設為60秒代表即使多次請求驗證碼，過了60秒才會發送第2次
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            // 監控電話驗證的狀態
+            .setCallbacks(verifyCallbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(phoneAuthOptions)
+    }
+
+    private val verifyCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks =
+        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            /** This callback will be invoked in two situations:
+             * 1 - Instant verification. In some cases the phone number can be instantly
+             * verified without needing to send or enter a verification code.
+             * 2 - Auto-retrieval. On some devices Google Play services can automatically
+             * detect the incoming verification SMS and perform verification without
+             * user action.  */
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(myTag, "onVerificationCompleted: $credential")
+            }
+
+            /**
+             * 發送驗證碼填入的電話號碼格式錯誤，或是使用模擬器發送都會產生發送錯誤，
+             * 使用模擬器發送會產生下列執行錯誤訊息：
+             * App validation failed. Is app running on a physical device?
+             */
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.e(myTag, "onVerificationFailed: ${e.message}")
+            }
+
+            /**
+             * The SMS verification code has been sent to the provided phone number,
+             * we now need to ask the user to enter the code and then construct a credential
+             * by combining the code with a verification ID.
+             */
+            override fun onCodeSent(id: String, token: PhoneAuthProvider.ForceResendingToken) {
+                Log.d(myTag, "onCodeSent: $id")
+                verificationId = id
+                resendToken = token
+            }
+        }
 }
 
