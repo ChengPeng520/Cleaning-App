dcb0f0f4e957ed559138b628d3d2a25e2110e47a
Victor <dkangel0422@gmail.com>
2023-06-08 22:19:57 +0800

    訂單進度新增自動刷新功能

--- app/src/main/java/com/example/cleaningapp/customer/fragment/OrderingFragment.kt
+++ app/src/main/java/com/example/cleaningapp/customer/fragment/OrderingFragment.kt
@@ -1,6 +1,8 @@
 package com.example.cleaningapp.customer.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +13,14 @@ import com.example.cleaningapp.R
 import com.example.cleaningapp.customer.detailed.Order
 import com.example.cleaningapp.customer.viewModel.OrderingViewModel
 import com.example.cleaningapp.databinding.FragmentVictorOrderingBinding
+import java.util.*
 
 class OrderingFragment : Fragment() {
     private lateinit var binding: FragmentVictorOrderingBinding
-    private lateinit var order: Order
+    private val timer = Timer()
+    private var isRefreshing = false
     val viewModel: OrderingViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,8 +33,33 @@ class OrderingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.getInt("orderId")?.let {
-            viewModel.fetchOrdersInfo(it)
+        arguments?.getInt("orderId")?.let { orderId ->
+            viewModel.fetchOrdersInfo(orderId)
+            startRefreshingOrderStatus(orderId)
         }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        stopRefreshingOrderStatus()
+    }
+
+    private fun startRefreshingOrderStatus(orderId: Int) {
+        isRefreshing = true
+        val handler = Handler(Looper.getMainLooper())
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    if (isRefreshing) {
+                        // 在这里执行从后端获取最新订单状态的逻辑
+                        viewModel.fetchOrdersInfo(orderId)
+                    }
+                }
+            }
+        }, 0, 3000)
+    }
+
+    private fun stopRefreshingOrderStatus() {
+        isRefreshing = false
+        timer.cancel()
     }
 }
--- app/src/main/java/com/example/cleaningapp/customer/fragment/OrderprogressFragment.kt
+++ app/src/main/java/com/example/cleaningapp/customer/fragment/OrderprogressFragment.kt
@@ -2,6 +2,8 @@ package com.example.cleaningapp.customer.fragment
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,11 +14,13 @@ import com.example.cleaningapp.customer.viewModel.OrderprogressViewModel
 import com.example.cleaningapp.R
 import com.example.cleaningapp.customer.detailed.Order
 import com.example.cleaningapp.databinding.FragmentVictorOrderprogressBinding
+import java.util.*
 
 class OrderprogressFragment : Fragment() {
     private lateinit var binding: FragmentVictorOrderprogressBinding
-    private lateinit var order: Order
     val viewModel: OrderprogressViewModel by viewModels()
+    private val timer = Timer()
+    private var isRefreshing = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,9 +34,34 @@ class OrderprogressFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.getInt("orderId")?.let {
-            viewModel.fetchOrdersInfo(it)
+        arguments?.getInt("orderId")?.let { orderId ->
+            viewModel.fetchOrdersInfo(orderId)
+            startRefreshingOrderStatus(orderId)
 
         }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        stopRefreshingOrderStatus()
+    }
+
+    private fun startRefreshingOrderStatus(orderId: Int) {
+        isRefreshing = true
+        val handler = Handler(Looper.getMainLooper())
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    if (isRefreshing) {
+                        // 在这里执行从后端获取最新订单状态的逻辑
+                        viewModel.fetchOrdersInfo(orderId)
+                    }
+                }
+            }
+        }, 0, 3000)
+    }
+
+    private fun stopRefreshingOrderStatus() {
+        isRefreshing = false
+        timer.cancel()
     }
 }
